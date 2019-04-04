db4ae0c1053245993bc17abd9998fe3f1bc9546f
theMiddleBlue <theMiddleBlue@users.noreply.github.com>
2019-04-04 15:35:52 +0200

    fix container

--- install-magento.sh
+++ install-magento.sh
@@ -2,6 +2,6 @@
 
 #su www-data <<EOSU
 
-/var/www/html/bin/magento setup:install --base-url=http://localhost:8080 --backend-frontname=admin --language=en_GB --timezone=Europe/Rome --currency=USD --db-host=mysql --db-name=magento --db-user=root --db-password=root --use-secure=0 --base-url-secure=0 --use-secure-admin=0 --admin-firstname=theMiddle --admin-lastname=theMiddle --admin-email=the@mi.ddle --admin-user=admin --admin-password=1qazxsw2
+/var/www/html/bin/magento setup:install --base-url=http://localhost:8888 --backend-frontname=admin --language=en_GB --timezone=Europe/Rome --currency=USD --db-host=mysql --db-name=magento --db-user=root --db-password=root --use-secure=0 --base-url-secure=0 --use-secure-admin=0 --admin-firstname=theMiddle --admin-lastname=theMiddle --admin-email=the@mi.ddle --admin-user=admin --admin-password=1qazxsw2
 
 #EOSU
--- stop.sh
+++ stop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker stop 22-sqli_web_1 22-sqli_mysql_1 && \
-docker rm 22-sqli_web_1 22-sqli_mysql_1 && \
+docker stop poc-magento227-sqli_web_1 poc-magento227-sqli_mysql_1 && \
+docker rm poc-magento227-sqli_web_1 poc-magento227-sqli_mysql_1 && \
 rm -rvf magento-exploits
